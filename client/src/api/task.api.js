6347db3b3a712513c23921150c2f332437679661
Jonathan Meza <practicantesena3@ticsocial.com.co>
2023-05-12 13:19:05 -0500

    Config DB

--- client/src/api/task.api.js
+++ client/src/api/task.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const tasksApi = axios.create({
-  baseURL: 'http://127.0.0.1:8000/tasks/api/v1/tasks/'
+  baseURL: 'python-react-production.up.railway.app/tasks/api/v1/tasks/'
 })
 
 // List all tasks
